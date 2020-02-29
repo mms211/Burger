94a38ee4fed0d7f011e226f84c1e001c02785ea8
Morgan Sommer <morgana.sommer@gmail.com>
2020-02-28 19:00:45 -0500

    basic burger.js formatted still need inputs

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,9 @@
-// * Inside `burger.js`, import `orm.js` into `burger.js`
+const orm = require("../config/orm.js");
 
 // * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+const burgers = {
 
-// * Export at the end of the `burger.js` file.
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burgers;
